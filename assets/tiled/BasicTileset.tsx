fa0717d80e97bacff3528cd96ddb87ccff0b231a
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2023-12-03 02:01:51 -0800

    initial commit

---
+++ assets/tiled/BasicTileset.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="BasicTileset" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../textures/tiles.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod tiled_fix;
+
+use bevy::prelude::*;
+use bevy_ecs_tilemap::TilemapPlugin;
+use tiled_fix::{TiledMapPlugin, TiledMapBundle};
+
+
+fn setup_graphics(mut commands: Commands, server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let tiled_map = server.load("tiled/test-1-export.tmx");
+
+    commands.spawn(TiledMapBundle {
+        tiled_map,
+        ..default()
+    });
+}
+
+
+fn main() {
+
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(TilemapPlugin)
+        .add_plugins(TiledMapPlugin)
+        .add_systems(Startup, setup_graphics)        
+        .run();
+}
